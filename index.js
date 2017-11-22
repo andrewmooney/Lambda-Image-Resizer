eb6dcbb8ba3da9dd08f181ed61a0de9b34723a55
andrewmooney <andrew.mooney@aamcommercial.com.au>
2017-11-22 11:31:55 +1000

    Updated node modules

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ const AWS = require('aws-sdk');
 const S3 = new AWS.S3();
 const Sharp = require('sharp');
 
+// Config
+const URL = 'https://s3-ap-southeast-2.amazonaws.com';
+
 exports.handler = (event, context, callback) => {
     // Get requested image and size from url pathParameters
-    const bucket = event.pathParameters.bucket;
-    const image = event.pathParameters.img;
-    const size = event.pathParameters.dimensions;
+    const bucket = event['pathParameters']['bucket'];
+    const image = event['pathParameters']['img'];
+    const size = event['pathParameters']['dimensions'];
     const dimensions = size.split('x');
     const width = parseInt(dimensions[0]);
     const height = parseInt(dimensions[1]);
+    const PATH = `${bucket}/${size}/${image}`;
 
     S3.getObject({
         Bucket: bucket, Key: image
@@ -30,9 +34,9 @@ exports.handler = (event, context, callback) => {
     }).promise())
     .then(() => {
         callback(null, {
-            statusCode: 200,
-            headers: { "x-custom-header": "Custom header value" },
-            body: `Resized ${bucket} ${image} to width: ${width} height: ${height}`
+            statusCode: 301,
+            headers: { 'location': `${URL}/${PATH}` },
+            body: ''
         })
     })
         .catch(err => callback(err))
